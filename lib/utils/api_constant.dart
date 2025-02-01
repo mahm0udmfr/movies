aa642ae4d0414465624554796d87e89dc39bbc5f
mahm0udmfr <mahmoud.mohamed.ram@gmail.com>
2025-02-02 00:52:50 +0200

    homeTab Is working well and do not need any thing login logic is working with cubit and interface some typo in name and files names fixed

--- lib/utils/api_constant.dart
+++ lib/utils/api_constant.dart
@@ -1,3 +1,4 @@
 class ApiConstant {
   static const String moviesBaseUrl = "yts.mx";
+  static const String userBaseUrl = "route-movie-apis.vercel.app";
 }
--- lib/utils/end_points.dart
+++ lib/utils/end_points.dart
@@ -1,4 +1,5 @@
 class EndPoints {
   static const String listMovies = "/api/v2/list_movies.json";
+  static const String login = "/auth/login";
 
 }
