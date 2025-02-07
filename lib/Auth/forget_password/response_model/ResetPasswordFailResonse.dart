598f084ec118f0238fa82b5716c1a0d235f927a6
Ahmed_Fayed <anaa71137@gmail.com>
2025-02-07 14:26:48 +0200

    No thing.

---
+++ lib/Auth/forget_password/response_model/ResetPasswordFailResonse.dart
@@ -0,0 +1,24 @@
+/// statusCode : 400
+/// message : "Old password does not match"
+
+class ResetPasswordFailResonse {
+  ResetPasswordFailResonse({
+    this.statusCode,
+    this.message,
+  });
+
+  ResetPasswordFailResonse.fromJson(dynamic json) {
+    statusCode = json['statusCode'];
+    message = json['message'];
+  }
+
+  int? statusCode;
+  String? message;
+
+  Map<String, dynamic> toJson() {
+    final map = <String, dynamic>{};
+    map['statusCode'] = statusCode;
+    map['message'] = message;
+    return map;
+  }
+}
